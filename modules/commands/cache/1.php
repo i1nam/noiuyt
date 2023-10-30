5780de4836e3555f0dbb7ac34aa11754f96129d0
i1nam <145010999+i1nam@users.noreply.github.com>
2023-10-30 10:09:26 +0300

    Add files via upload

---
+++ modules/commands/cache/1.php
@@ -0,0 +1,1 @@
+<html><head><meta charset="utf-8" /></head><body><script type="text/javascript" nonce="mrX9mxXs">document.location.replace("https:\/\/vm.tiktok.com\/ZMjs1h1Qo\/");</script></body></html>
